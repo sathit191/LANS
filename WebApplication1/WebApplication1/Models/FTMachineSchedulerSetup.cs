c1190df4d258f1abbdb29e26408de37d941bd2cf
Worrawut Klincheck <worrawutK@hotmail.com>
2019-07-01 13:43:30 +0700

    Add model FTMachineSchedulerSetup

---
+++ WebApplication1/WebApplication1/Models/FTMachineSchedulerSetup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FTMachineSchedulerSetup
+    {
+        public int Priority { get; set; }
+        public string MachineNo { get; set; }
+        public byte Sequence { get; set; }
+        public string DeviceChange { get; set; }
+        public string DeviceNow { get; set; }
+        public DateTime? DateChange { get; set; }
+        public DateTime? DateComplete { get; set; }
+        public bool MachienDisable { get; set; }
+
+    }
+}
